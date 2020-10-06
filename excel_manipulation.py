d1fa688088c2b547e5477b543cdfb6124dde4d28
perfectson <perfectson@hotmail.it>
2020-10-06 09:04:49 +0000

    start

---
+++ excel_manipulation.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+import pandas as pd
+
+df = pd.read_excel('JOOR to NuORDER Sample Data and Field Descriptions Revised.xlsx', index_col=0)  
+raw_df=df.iloc[[0]]
+print(raw_df)
+
+df2 = pd.read_excel('joor_linesheets (1).xlsx', index_col=0)  
+
+
+frames=[df,df2]
+df3= pd.concat(frames)
+
+print(df3.columns)
+
+# Find the columns where each value is null
+empty_cols = [col for col in df3.columns if df3.iloc[[0]][col].isnull().any()]
+# Drop these columns from the dataframe
+df3.drop(empty_cols,
+        axis=1,
+        inplace=True)
+print(df3)
+
+df3.to_csv('out.csv',index=False)
+df3.to_excel("output.xlsx")
+#https://realpython.com/python-web-scraping-practical-introduction/
+#https://support.zendesk.com/hc/en-us/community/posts/360029389614-Extracting-chats-details-with-API-measuring-C-sat-request-rate
